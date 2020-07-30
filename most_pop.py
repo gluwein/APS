0a2acad854d1c07fff268471fb8946833b964a3d
성정욱 <tjdwjddnr296@naver.com>
2020-07-30 17:58:42 +0900

    Counter

--- most_pop.py
+++ most_pop.py
@@ -1,7 +1,9 @@
 #최빈값을 구한다. 
 # 범위 -> List 순회
 # 찾을 값은 순회하는 수 중의 하나를 고름
-numbers = input().split()
-print(numbers)
+
 # for number in numbers:
 #     my_count = numbers.count(number)
+from collections import Counter 
+colors = ['red', 'blue', 'red', 'green', 'blue', 'blue']
+print(Counter(colors))
