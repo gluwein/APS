a4dab526e7edec7b076d953a581844c0986f2ac5
성정욱 <tjdwjddnr296@naver.com>
2020-09-08 19:58:17 +0900

    !

--- swea D2/swea_1959_두개의숫자열.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for i in range(1,T+1):
-    m,n = list(map(int,input().split()))
-    if m>n:
-        for j in range(m-n):
-            for i in range(len(m)):
-                result.append(m[i+j]*n[i])
